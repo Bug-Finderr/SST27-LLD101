692a1cf618f54e4accd4453d629dbc509ace7585
Bug-Finderr <sudharsan.23bcs10077@ms.sst.scaler.com>
2025-09-03 15:58:53 +0530

    refactor: employee-adapter-java-sources

---
+++ employee-adapter-java-sources/exercise/src/main/java/exercise/EmployeeLDAPAdapter.java
@@ -0,0 +1,21 @@
+package exercise;
+
+import java.util.Objects;
+
+public class EmployeeLDAPAdapter implements Employee {
+    private final EmployeeLDAP ldap;
+
+    public EmployeeLDAPAdapter(EmployeeLDAP ldap) { this.ldap = Objects.requireNonNull(ldap, "ldap"); }
+
+    @Override
+    public String getId() { return ldap.get("uid"); }
+
+    @Override
+    public String getFirstName() { return ldap.get("givenName"); }
+
+    @Override
+    public String getLastName() { return ldap.get("sn"); }
+
+    @Override
+    public String getEmail() { return ldap.get("mail"); }
+}
